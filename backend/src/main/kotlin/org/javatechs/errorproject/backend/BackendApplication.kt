11c075be310f3ddb869a2aa19b58e3b7cf3b7b29
Gleb Klochkov <gklochkov@moysklad.ru>
2017-09-26 12:36:28 +0300

    Ok. It works for now ^)

--- backend/src/main/kotlin/org/javatechs/errorproject/backend/BackendApplication.kt
+++ backend/src/main/kotlin/org/javatechs/errorproject/backend/BackendApplication.kt
@@ -2,8 +2,10 @@ package org.javatechs.errorproject.backend
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 
 @SpringBootApplication
+@EntityScan("org.javatechs.errorproject.backend.node")
 class BackendApplication{
 
 }
---
+++ backend/src/main/kotlin/org/javatechs/errorproject/backend/node/testNode.kt
@@ -0,0 +1,9 @@
+package org.javatechs.errorproject.backend.node
+
+import org.neo4j.ogm.annotation.NodeEntity
+
+@NodeEntity
+class testNode {
+    var id: Long? = null
+
+}
